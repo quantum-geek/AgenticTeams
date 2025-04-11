36a811b991e83e97772afb67338426241e3fc673
quantum-geek <impatientastronomer@gmail.com>
2025-04-11 13:57:08 -0700

    adding multiple agents to drive different functionalities

--- FinanceAgent.py
+++ FinanceAgent.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 '''
+##Just a test tro see if the agent grabs an unknown symbol. 
 def get_company_symbol(company_name: str) -> str:
     company_symbol = {
         "MSFT": "Phidata",
@@ -23,7 +24,7 @@ agent = Agent(
     show_tool_calls=True, 
     markdown=True,
     instructions=['You must use tables to display the data.', 'If you do not have the company symbol, you can use the get_company_symbol function to get it.'
-    'even if it is not a public company.'],
+    'even if it is not a public company. Can you also make sure to include the company name in the table? I will also appreciate if you use dotted lines to separate the tables.',],
 
     #debug_mode=True,
 )
